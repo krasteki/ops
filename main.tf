d1def2f35d40de562e613d7afcfe54f74e8b3425
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-08-14 13:28:41 +0300

    adding another repository

--- main.tf
+++ main.tf
@@ -13,3 +13,8 @@ resource "github_repository" "example" {
   name        = "example"
   description = "My awesome codebase"
 }
+
+resource "github_repository" "example2" {
+  name        = "example2"
+  description = "My awesome codebase2"
+}
